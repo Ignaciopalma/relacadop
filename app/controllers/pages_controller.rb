f3ac1de8ab26674298fb307ae98df2545cf5ed58
Ignacio Palma <ignacio.palma.@live.com>
2016-10-25 00:11:14 -0300

    hope to be final

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,8 @@ class PagesController < ApplicationController
 
 	def contact
 	end
+
+	def rakelita
+  		@messages = Message.all
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'marco_legal/derechos_nino' => 'marco_legal#derechos_nino'
   resources :messages
 
+  get 'pages/rakelita' => 'pages#rakelita'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
