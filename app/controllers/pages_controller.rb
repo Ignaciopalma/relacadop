25b811ba4fe64de665b3e1b69ef42010dc680557
Ignacio Palma <ignacio.palma.@live.com>
2016-11-15 01:07:09 -0300

    alimentacion de pagina casi lista, faltan estilo y agregar dos fotos carrierwave

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,16 @@
 class PagesController < ApplicationController
 	def index
+		@lastFourReports = Report.order(id: :desc).limit(4)
 	end
 
 	def about
 	end
 
 	def contact
+	end
+
+	def noticias
+		@reports = Report.all
 	end
 
 	def rakelita
