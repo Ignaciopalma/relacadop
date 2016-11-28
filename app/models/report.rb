4dbad2928bfa0671f4857ae1ef301fb9943fff1d
Ignacio Palma <ignacio.palma.@live.com>
2016-11-27 21:40:36 -0300

    fixing images uploaders

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,10 +2,10 @@ class Report < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	mount_uploader :image_two, ImageTwoUploader
 	mount_uploader :image_three, ImageThreeUploader
-	mount_uploader :image_four, ImageThreeUploader
-	mount_uploader :image_five, ImageThreeUploader
-	mount_uploader :image_six, ImageThreeUploader
-	mount_uploader :image_seven, ImageThreeUploader
-	mount_uploader :image_eight, ImageThreeUploader
-	mount_uploader :image_nine, ImageThreeUploader
+	mount_uploader :image_four, ImageFourUploader
+	mount_uploader :image_five, ImageFiveUploader
+	mount_uploader :image_six, ImageSixUploader
+	mount_uploader :image_seven, ImageSevenUploader
+	mount_uploader :image_eight, ImageEightUploader
+	mount_uploader :image_nine, ImageNineUploader
 end
