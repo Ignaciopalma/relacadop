d0f5cdc3f8ba3608b375ecb2eb78a20dedf1609c
Ignacio Palma <ignacio.palma.@live.com>
2016-11-28 11:57:34 -0300

    change endpoints host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                        # required
-    region:                'us-west-1',  
-    host:                  's3-us-west-1.amazonaws.com',             # optional, defaults to nil                # optional, defaults to 'us-east-1'
+    region:                'us-east-1',  
+    host:                  's3-us-east-1.amazonaws.com',             # optional, defaults to nil                # optional, defaults to 'us-east-1'
     endpoint:              's3.amazonaws.com' # optional, defaults to nil
   }
   config.fog_directory  = ENV["AWS_BUCKET_NAME"]                          # required
