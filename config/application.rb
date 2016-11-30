3c9c02224eafbf2e84207eab7f1cb1f81fe67550
Ignacio Palma <ignacio.palma.@live.com>
2016-11-30 14:19:05 -0300

    common

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-
+config.assets.precompile += Ckeditor.assets
+config.assets.precompile += %w( ckeditor/* )
+config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -20,9 +22,7 @@ module Relacadop
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w( ckeditor/* )
-    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
